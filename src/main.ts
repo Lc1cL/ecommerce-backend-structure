109c3d7452b20b2a762d1e9fe96414b70ebe2fc0
unknown <lolacamposcipo@gmail.com>
2024-09-05 15:07:06 -0300

    define DB_HOST AND DB_PORT

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -18,7 +20,7 @@ async function bootstrap() {
   const document= SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
   
-  await app.listen(3000);
+  await app.listen(PORT);
   console.log('Server listening on port 3000')
 }
 bootstrap();
